74b3cef5ce66d6888135ee9c6682b2f3bf5539e1
samuelc <samuelnasc.dev@gmail.com>
2024-07-12 12:05:22 -0300

    Subindo os primeiros arquivos e configurações

---
+++ src/main/java/com/project/bibliotecapp/BibliotecappApplication.java
@@ -0,0 +1,13 @@
+package com.project.bibliotecapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecappApplication.class, args);
+	}
+
+}
